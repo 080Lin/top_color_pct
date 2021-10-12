a58c742dec9732d54e4b0522d7961b56d4ebb6ce
Naomikka <91838588+NasciNSC@users.noreply.github.com>
2021-10-12 08:19:35 +0300

    MD test

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import find_packages, setup
 
+long_description = ''
+
 setup(
     name='top_colors_pct',
     packages=find_packages(),
     version='0.1.0',
     description='This library allows you to take N top colors from the image',
     author='NasciNSC',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     install_requires=['webcolors'],
     keywords=['python', 'image', 'processing', 'img', 'top_colors', 'color'],
